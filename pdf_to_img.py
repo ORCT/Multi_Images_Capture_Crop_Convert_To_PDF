f04922adc8a8c46436998146e943df4a9f83a05d
ORCT <sonic407@naver.com>
2021-10-27 23:18:01 +0900

    add gitignore

---
+++ pdf_to_img.py
@@ -0,0 +1,7 @@
+import os
+from pdf2image import convert_from_path
+file_list = os.listdir("./").endswith('.pdf')
+print(file_list)
+#pages = convert_from_path('pdf_file', 500)
+#for page in pages:
+#    page.save('out.png', 'PNG')
