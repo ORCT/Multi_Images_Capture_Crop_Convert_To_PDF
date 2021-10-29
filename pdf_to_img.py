3f8852a0d6f9d6691c889689669804db1783ec82
ORCT <sonic407@naver.com>
2021-10-29 21:49:20 +0900

    edit

--- pdf_to_img.py
+++ pdf_to_img.py
@@ -1,6 +1,6 @@
 import os
 from pdf2image import convert_from_path
-file_list = os.listdir("./").endswith('.pdf')
+file_list = os.listdir("./")
 print(file_list)
 #pages = convert_from_path('pdf_file', 500)
 #for page in pages:
